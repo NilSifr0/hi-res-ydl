54dc920f90c0df1529772fb740fce834976540e3
Jimmy II Caticat <jimmyiicaticat@gmail.com>
2025-02-16 02:40:47 +0800

    feat: added progress bar for dle

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import os
 import subprocess
+from time import sleep
 
 import ffmpeg
 import PySimpleGUI as sg
 from pytubefix import YouTube
-from pytubefix.cli import on_progress
+
+# from pytubefix.cli import on_progress
 
 
-def pull_stream(yt_url):
-    yt = YouTube(yt_url, on_progress_callback=on_progress)
+def dl_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    dl_progress_bar(bytes_downloaded, total_size)
+    # percentage_of_completion = bytes_downloaded / total_size * 100
+    # print(f"{round(percentage_of_completion, 2)} %")
+
+
+def dl_progress_bar(bytes_downloaded, total_size):
+    dl_pb.update(bytes_downloaded, total_size)
+
+
+def pull_stream(yt_url, status):
+    status.update("Status: Downloading")
+    yt = YouTube(yt_url, on_progress_callback=dl_progress)
     # get highest quality of video and audio
-    video_stream = (
-        yt.streams.filter(
-            adaptive="True",
-            only_video="True",
-            file_extension="mp4",
-        )
-        .order_by("resolution")
-        .desc()
-        .first()
-    )
+    # video_stream = (
+    #     yt.streams.filter(
+    #         adaptive="True",
+    #         only_video="True",
+    #         file_extension="mp4",
+    #     )
+    #     .order_by("resolution")
+    #     .desc()
+    #     .first()
+    # )
+
+    video_stream = yt.streams.filter(
+        adaptive=True,
+        only_video=True,
+        file_extension="mp4",
+        resolution="2160p",
+        fps=60,
+    ).first()
     audio_stream = (
         yt.streams.filter(
             adaptive="True",
@@ -33,6 +56,9 @@ def pull_stream(yt_url):
     video_input = video_stream.download()
     audio_input = audio_stream.download()
     title = yt.title
+
+    status.update("Status: Compiling")
+
     return video_input, audio_input, title
 
 
@@ -50,7 +76,8 @@ def clean_input(v_stream, a_stream):
         print(f"Error: {e.filename} {e.strerror}")
 
 
-def process_stream(video_input, audio_input, title):
+def process_stream(video_input, audio_input, title, status):
+    sleep(10)
     v_input = ffmpeg.input(video_input).node.short_repr
     a_input = ffmpeg.input(audio_input).node.short_repr
     input_str = f'-i "{v_input}" -i "{a_input}"'
@@ -71,22 +98,40 @@ def app_interface():
             sg.Input(do_not_clear=False, k="-URL-"),
             sg.Button("Get", k="-GET-"),
         ],
+        [sg.Text("Status: ", k="-STATUS-")],
+        [
+            sg.ProgressBar(
+                k="-DLP-",
+                expand_x=True,
+                orientation="h",
+                max_value=None,
+                bar_color=("DeepSkyBlue", "azure"),
+                s=(0, 20),
+            )
+        ],
     ]
 
     window = sg.Window("HR-YDL", layout)
 
-    # sg.theme_previewer()
-    # theme_list = sg.theme_list()
-    # print(theme_list)
+    status = window["-STATUS-"]
+    global dl_pb
+    dl_pb = window["-DLP-"]
+
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
-
-        if event == "-GET-":
+        elif event == "-GET-":
             output_directory()
-            video_input, audio_input, title = pull_stream(values["-URL-"])
-            process_stream(video_input, audio_input, title)
+            video_input, audio_input, title = pull_stream(
+                values["-URL-"],
+                status,
+            )
+
+            process_stream(video_input, audio_input, title, status)
+            window["-STATUS-"].update("Status: Done")
+        else:
+            pass
 
     window.close()
 
